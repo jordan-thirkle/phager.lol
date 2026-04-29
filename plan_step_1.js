1cee62cff69dcf5fb3e7d5844815f865a474d597
jordan thirkle <jordantthirkle@gmail.com>
2026-04-29 23:41:45 +0100

    chore: remove test runner scratch files

--- plan_step_1.js
+++
@@ -1,1 +0,0 @@
-console.log('Done step 1.');
--- test_runner.js
+++
@@ -1,9 +0,0 @@
-const { execSync } = require('child_process');
-
-try {
-  execSync('node --test client/src/ui/meta.test.js', { stdio: 'inherit' });
-  console.log('Tests passed.');
-} catch (e) {
-  console.error('Tests failed.');
-  process.exit(1);
-}
